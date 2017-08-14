wakiyamap/btcd

chaincfg+integration: add BIP0009 deployment parameters for segwit
This commit adds set of BIP0009 (Version Bits) deployment parameters
for all networks detailing the activation parameters for the segwit
soft-fork.

Additionally, the BIP0009 integration test has been updated to test for
the proper transitioning of version bits state for the segwit soft
fork. Finally, the `getblockchaininfo` test has also been updated to
properly display the state of segwit.

integration/bip0009_test.go
@@ -299,6 +299,7 @@ func TestBIP0009(t *testing.T) {
 	t.Parallel()
 
 	testBIP0009(t, "dummy", chaincfg.DeploymentTestDummy)
+	testSegwit(t, "segwit", chaincfg.DeploymentSegwit)
 }
 
 // TestBIP0009Mining ensures blocks built via btcd's CPU miner follow the rules

treyward32/pearl

node/integration/bip0009_test.go
@@ -299,6 +299,7 @@ func TestBIP0009(t *testing.T) {
 	t.Parallel()
 
 	testBIP0009(t, "dummy", chaincfg.DeploymentTestDummy)
+	testBIP0009(t, "segwit", chaincfg.DeploymentSegwit)
 }
 
 // TestBIP0009Mining ensures blocks built via btcd's CPU miner follow the rules