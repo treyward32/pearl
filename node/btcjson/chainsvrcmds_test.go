aakselrod/btcd

multi: use hidden varint for cftypes count; make filter type enum, not uint8

btcjson/chainsvrcmds_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 
 	"github.com/roasbeef/btcd/btcjson"
+	"github.com/roasbeef/btcd/wire"
 )
 
 // TestChainSvrCmds tests all of the chain server commands marshal and unmarshal
@@ -320,27 +321,33 @@ func TestChainSvrCmds(t *testing.T) {
 		{
 			name: "getcfilter",
 			newCmd: func() (interface{}, error) {
-				return btcjson.NewCmd("getcfilter", "123", 0)
+				return btcjson.NewCmd("getcfilter", "123",
+					wire.GCSFilterExtended)
 			},
 			staticCmd: func() interface{} {
-				return btcjson.NewGetCFilterCmd("123", 0)
+				return btcjson.NewGetCFilterCmd("123",
+					wire.GCSFilterExtended)
 			},
-			marshalled: `{"jsonrpc":"1.0","method":"getcfilter","params":["123",0],"id":1}`,
+			marshalled: `{"jsonrpc":"1.0","method":"getcfilter","params":["123",1],"id":1}`,
 			unmarshalled: &btcjson.GetCFilterCmd{
-				Hash: "123",
+				Hash:       "123",
+				FilterType: wire.GCSFilterExtended,
 			},
 		},
 		{
 			name: "getcfilterheader",
 			newCmd: func() (interface{}, error) {
-				return btcjson.NewCmd("getcfilterheader", "123", 0)
+				return btcjson.NewCmd("getcfilterheader", "123",
+					wire.GCSFilterExtended)
 			},
 			staticCmd: func() interface{} {
-				return btcjson.NewGetCFilterHeaderCmd("123", 0)
+				return btcjson.NewGetCFilterHeaderCmd("123",
+					wire.GCSFilterExtended)
 			},
-			marshalled: `{"jsonrpc":"1.0","method":"getcfilterheader","params":["123",0],"id":1}`,
+			marshalled: `{"jsonrpc":"1.0","method":"getcfilterheader","params":["123",1],"id":1}`,
 			unmarshalled: &btcjson.GetCFilterHeaderCmd{
-				Hash: "123",
+				Hash:       "123",
+				FilterType: wire.GCSFilterExtended,
 			},
 		},
 		{

treyward32/pearl

node/btcjson/chainsvrcmds_test.go
@@ -12,6 +12,7 @@ import (
 	"testing"
 
 	"github.com/btcsuite/btcd/btcjson"
+	"github.com/btcsuite/btcd/wire"
 )
 
 // TestChainSvrCmds tests all of the chain server commands marshal and unmarshal
@@ -320,27 +321,33 @@ func TestChainSvrCmds(t *testing.T) {
 		{
 			name: "getcfilter",
 			newCmd: func() (interface{}, error) {
-				return btcjson.NewCmd("getcfilter", "123", 0)
+				return btcjson.NewCmd("getcfilter", "123",
+					wire.GCSFilterExtended)
 			},
 			staticCmd: func() interface{} {
-				return btcjson.NewGetCFilterCmd("123", 0)
+				return btcjson.NewGetCFilterCmd("123",
+					wire.GCSFilterExtended)
 			},
-			marshalled: `{"jsonrpc":"1.0","method":"getcfilter","params":["123",0],"id":1}`,
+			marshalled: `{"jsonrpc":"1.0","method":"getcfilter","params":["123",1],"id":1}`,
 			unmarshalled: &btcjson.GetCFilterCmd{
-				Hash: "123",
+				Hash:       "123",
+				FilterType: wire.GCSFilterExtended,
 			},
 		},
 		{
 			name: "getcfilterheader",
 			newCmd: func() (interface{}, error) {
-				return btcjson.NewCmd("getcfilterheader", "123", 0)
+				return btcjson.NewCmd("getcfilterheader", "123",
+					wire.GCSFilterExtended)
 			},
 			staticCmd: func() interface{} {
-				return btcjson.NewGetCFilterHeaderCmd("123", 0)
+				return btcjson.NewGetCFilterHeaderCmd("123",
+					wire.GCSFilterExtended)
 			},
-			marshalled: `{"jsonrpc":"1.0","method":"getcfilterheader","params":["123",0],"id":1}`,
+			marshalled: `{"jsonrpc":"1.0","method":"getcfilterheader","params":["123",1],"id":1}`,
 			unmarshalled: &btcjson.GetCFilterHeaderCmd{
-				Hash: "123",
+				Hash:       "123",
+				FilterType: wire.GCSFilterExtended,
 			},
 		},
 		{