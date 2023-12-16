kcalvinalvin/btcd

blockchain, netsync, main, cmd/addblock: Use utxocache
This change is part of the effort to add utxocache support to btcd.

utxo cache is now used by the BlockChain struct.  By default it's used
and the minimum cache is set to 250MiB.  The change made helps speed up
block/tx validation as the cache allows for much faster lookup of utxos.
The initial block download in particular is improved as the db i/o
bottleneck is remedied by the cache.

cmd/addblock/import.go
@@ -287,6 +287,16 @@ func (bi *blockImporter) Import() chan *importResults {
 	// the status handler when done.
 	go func() {
 		bi.wg.Wait()
+
+		// Flush the changes made to the blockchain.
+		log.Info("Flushing blockchain caches to the disk...")
+		if err := bi.chain.FlushCachedState(blockchain.FlushRequired); err != nil {
+			log.Errorf("Error while flushing the blockchain state: %v", err)
+			bi.errChan <- err
+			return
+		}
+		log.Info("Done flushing blockchain caches to disk")
+
 		bi.doneChan <- true
 	}()
 

treyward32/pearl

node/cmd/addblock/import.go
@@ -287,6 +287,16 @@ func (bi *blockImporter) Import() chan *importResults {
 	// the status handler when done.
 	go func() {
 		bi.wg.Wait()
+
+		// Flush the changes made to the blockchain.
+		log.Info("Flushing blockchain caches to the disk...")
+		if err := bi.chain.FlushUtxoCache(blockchain.FlushRequired); err != nil {
+			log.Errorf("Error while flushing the blockchain state: %v", err)
+			bi.errChan <- err
+			return
+		}
+		log.Info("Done flushing blockchain caches to disk")
+
 		bi.doneChan <- true
 	}()
 